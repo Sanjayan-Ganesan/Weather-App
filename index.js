a6eccc193158086b4961e648ccedfe4343dabe88
Sanjayan <97502651+Sanjayan-Ganesan@users.noreply.github.com>
2022-03-19 22:29:49 +0530

    commit

--- index.js
+++ index.js
@@ -5,17 +5,26 @@ var map_container = document.getElementById("map_container");
 
 var main_container = document.getElementById("container");
 
+
+
+var forecast = document.getElementById("forecast");
+var key = "56a0f5000c489d8dff3b8924440ee521";
 async function search(){
-    var key = "56a0f5000c489d8dff3b8924440ee521";
+    
+   
     var name = document.getElementById("name").value;
     let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${key}&units=metric`);
     let data = await res.json();
+
     //console.log(data);
     append_data(data);
+    forecast_data(data);
+    //dayforcast(data);
 }
 //search();
 
-
+var lat;
+var lon;
 
 function append_data(data){
     console.log(data);
@@ -33,12 +42,12 @@ document.getElementById("map_container").innerHTML = "";
         city_name.style.color = `white`;
         city_name.style.marginLeft = `36.25%`
 
-        let lat= document.createElement("h6");
+        lat = document.createElement("h6");
         lat.textContent = `Latitude: ${data.coord.lat}°`;
         lat.style.color = `white`;
         lat.style.marginLeft = `15%`
 
-        let lon= document.createElement("h6");
+        lon = document.createElement("h6");
         lon.textContent = `Longitude: ${data.coord.lon}°`;
         lon.style.color = `white`;
         lon.style.marginTop = `-10.65%`;
@@ -99,6 +108,8 @@ document.getElementById("map_container").innerHTML = "";
         main_container.append(map_container,container);
 
 
+
+
   
 }
 
@@ -113,3 +124,58 @@ temp: 14.56
 temp_max: 16.02
 temp_min: 13.23
 */
+
+async function forecast_data(data){
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+try{
+    let result = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={hourly}&appid=${key}&units=metric`);
+
+    let datum = await result.json();
+
+    //console.log(datum);
+    append_to_day(datum);
+}catch(error){
+    console.log(error);
+}
+    
+}
+
+
+
+function append_to_day(datum){
+console.log(datum);
+
+datum.daily.map(function(element){
+    var forecast_div = document.createElement("div");
+
+    var hum = document.createElement("h3");
+    hum.textContent = `Humidity:${element.humidity}%`;
+    hum.style.color = `white`;
+
+
+    var mrng_temp = document.createElement("h4");
+    mrng_temp.textContent = `Temp:${element.temp.max}°C`;
+    mrng_temp.style.color = `white`;
+    var pic = document.createElement("img");
+    pic.style.width = `150px`;
+
+
+    if(element.temp.max >= 20){
+     
+    pic.src = "https://ak.picdn.net/shutterstock/videos/1021489846/thumb/2.jpg";
+
+    }else if(element.temp.max <= 20){
+        pic.src =  "https://media.istockphoto.com/videos/beautiful-animation-of-digital-clouds-on-a-black-background-alpha-video-id900212980?s=480x480";
+    }else if(element.temp.max <= 0){
+        pic.src = "https://ak.picdn.net/shutterstock/videos/20929234/thumb/1.jpg";
+
+    }
+
+    
+
+
+    forecast_div.append(hum,mrng_temp,pic);
+    forecast.append(forecast_div);
+})
+}
